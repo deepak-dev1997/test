769c168273457c9ec6e20db69374502220d12849
deepak.devdd.97@gmail.com <U@tusees@#1997>
2025-01-15 00:20:35 +0530

    .

--- model2.py
+++ model2.py
@@ -1,61 +1,72 @@
-# model.py
-
-from tf_keras import Model
-from tf_keras import Input
-from tf_keras.layers import Conv2D , MaxPooling2D, concatenate , Conv2DTranspose, Dropout
+# model2.py
+from tf_keras import Model, Input
+from tf_keras.layers import Conv2D, MaxPooling2D, concatenate, Conv2DTranspose, Dropout, BatchNormalization
 from tf_keras.optimizers import Adam
 
-def unet(input_size=(256, 256, 4), num_classes=21):  # Updated num_classes to 10
+def unet(input_size=(256, 256, 4), num_classes=10):
     inputs = Input(input_size)
     
-    # Encoder
+    # Encoder with BatchNormalization
     c1 = Conv2D(64, 3, activation='relu', padding='same')(inputs)
+    c1 = BatchNormalization()(c1)
     c1 = Conv2D(64, 3, activation='relu', padding='same')(c1)
+    c1 = BatchNormalization()(c1)
     p1 = MaxPooling2D()(c1)
     
     c2 = Conv2D(128, 3, activation='relu', padding='same')(p1)
+    c2 = BatchNormalization()(c2)
     c2 = Conv2D(128, 3, activation='relu', padding='same')(c2)
+    c2 = BatchNormalization()(c2)
     p2 = MaxPooling2D()(c2)
     
     c3 = Conv2D(256, 3, activation='relu', padding='same')(p2)
+    c3 = BatchNormalization()(c3)
     c3 = Conv2D(256, 3, activation='relu', padding='same')(c3)
+    c3 = BatchNormalization()(c3)
     p3 = MaxPooling2D()(c3)
     
     c4 = Conv2D(512, 3, activation='relu', padding='same')(p3)
+    c4 = BatchNormalization()(c4)
     c4 = Conv2D(512, 3, activation='relu', padding='same')(c4)
+    c4 = BatchNormalization()(c4)
     p4 = MaxPooling2D()(c4)
     
     # Bottleneck
     c5 = Conv2D(1024, 3, activation='relu', padding='same')(p4)
+    c5 = BatchNormalization()(c5)
     c5 = Conv2D(1024, 3, activation='relu', padding='same')(c5)
+    c5 = BatchNormalization()(c5)
     
-    # Decoder
+    # Decoder with BatchNormalization
     u6 = Conv2DTranspose(512, 2, strides=(2, 2), padding='same')(c5)
     u6 = concatenate([u6, c4])
     c6 = Conv2D(512, 3, activation='relu', padding='same')(u6)
+    c6 = BatchNormalization()(c6)
     c6 = Conv2D(512, 3, activation='relu', padding='same')(c6)
+    c6 = BatchNormalization()(c6)
     
     u7 = Conv2DTranspose(256, 2, strides=(2, 2), padding='same')(c6)
     u7 = concatenate([u7, c3])
     c7 = Conv2D(256, 3, activation='relu', padding='same')(u7)
+    c7 = BatchNormalization()(c7)
     c7 = Conv2D(256, 3, activation='relu', padding='same')(c7)
+    c7 = BatchNormalization()(c7)
     
     u8 = Conv2DTranspose(128, 2, strides=(2, 2), padding='same')(c7)
     u8 = concatenate([u8, c2])
     c8 = Conv2D(128, 3, activation='relu', padding='same')(u8)
+    c8 = BatchNormalization()(c8)
     c8 = Conv2D(128, 3, activation='relu', padding='same')(c8)
+    c8 = BatchNormalization()(c8)
     
     u9 = Conv2DTranspose(64, 2, strides=(2, 2), padding='same')(c8)
     u9 = concatenate([u9, c1])
     c9 = Conv2D(64, 3, activation='relu', padding='same')(u9)
+    c9 = BatchNormalization()(c9)
     c9 = Conv2D(64, 3, activation='relu', padding='same')(c9)
+    c9 = BatchNormalization()(c9)
     
-    outputs = Conv2D(num_classes, 1, activation='softmax')(c9)  # Updated filters to num_classes
+    outputs = Conv2D(num_classes, 1, activation='softmax')(c9)
     
     model = Model(inputs=[inputs], outputs=[outputs])
-    
-    model.compile(optimizer=Adam(learning_rate=1e-4), 
-                  loss='categorical_crossentropy', 
-                  metrics=['accuracy'])
-    
     return model
