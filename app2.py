96543780cf6f7febbe54527cfa483ea57bf49090
deepak.devdd.97@gmail.com <U@tusees@#1997>
2025-02-24 08:46:34 +0530

    obstruction data

--- app2.py
+++ app2.py
@@ -156,4 +156,4 @@ def mask_file(filename):
     return send_from_directory(app.config['MASK_FOLDER'], filename)
 
 if __name__ == '__main__':
-    app.run(debug=False, host="0.0.0.0")
+    app.run(debug=False, host="0.0.0.0",port=5001)
